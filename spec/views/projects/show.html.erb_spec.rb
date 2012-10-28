3a225624fc3921ef6330e1226010392449cb1c3e
DukeFruitarian <Duke1@sura.ru>
2012-10-28 12:26:33 +0400

    changes in a view projects#show

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -15,7 +15,6 @@ describe "projects/show" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Title/)
     rendered.should match(/MyText/)
-    rendered.should match(/1/)
     rendered.should match(/false/)
   end
 end
