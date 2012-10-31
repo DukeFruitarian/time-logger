1583bfb6767a425e25ac4dc85192179eea4b1906
DukeFruitarian <Duke1@sura.ru>
2012-10-31 14:27:34 +0400

    add some css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ TimeLogger::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['time_logger.css', 'scaffolds.css']
+  # config.assets.precompile += ['time_logger.css', 'scaffolds.css']
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
